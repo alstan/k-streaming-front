f6076704740e8fb9bb7e9cc782b5f678d6b7a6c7
albustan <stanley.albuquerque@joyjet.com>
2022-04-09 13:27:18 -0300

    ester excercise 1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import MainMenu from './components/MainMenu';
-import Counter from './components/Counter';
-import PersonalData from './components/PersonalData';
-
 function App() {
-  const date = "31/03/2000";
+  const user = {};
 
   return (
     <div className="App">
-      <MainMenu time={date} />
-      <Counter />
-      <PersonalData />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
